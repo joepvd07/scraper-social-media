00fc13c86715bbd127b2e7669461804adc907d2f
JOEPJOEPPRO <97831753+joepvd07@users.noreply.github.com>
2022-02-08 10:05:25 +0000

    First test

---
+++ scratch.py
@@ -0,0 +1,7 @@
+wich_social = int(input("WICH SOCIAL:\nINSTAGRAM      1\nFACEBOOK       2\nALL            5"))
+instagram_url = "https://www.instagram.com/"
+
+if wich_social.find("1") == 0:
+  print("p")
+  
+  
